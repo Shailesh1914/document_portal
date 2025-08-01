87ffbcbda256f360ade26c54b7ea76d1713fed5a
shailesh1914 <shailesh1914@hotmail.com>
2025-08-01 23:26:28 +0530

    model_loader code is implemented

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -39,4 +39,62 @@ class ModelLoader:
             raise DocumentPortalException("Failed to load embedding model", sys)
     
     def load_llm(self):
-        pass
+        """Load and return the LLM model"""
+        """Load LLM dynamically based on provider in config"""
+        llm_block=self.config["llm"]
+        
+        log.info("Loading LLM ...")
+        
+        # Default provider 
+        provider_key = os.getenv("LLM_PROVIDER", "groq")   # Default groq
+        
+        if provider_key not in llm_block:
+            log.error("LLM provider not found in config", provider_key=provider_key)
+            raise ValueError(f"Provider '{provider_key}' not found in config")
+        
+        llm_config = llm_block[provider_key]
+        provider = llm_config.get("provider")
+        model_name = llm_config.get("model_name")
+        temperature = llm_config.get("temperature", 0.2)
+        max_tokens = llm_config.get("max_output_token", 2048)
+        
+        log.info("Loading LLM", provider=provider, model=model_name, temperature=temperature, max_tokens=max_tokens)
+        
+        if provider == "google":
+            llm=ChatGoogleGenerativeAI(
+                model=model_name,
+                temperature=temperature,
+                max_output_token=max_tokens                
+            )
+            return llm
+        
+        elif provider == "groq":
+            llm=ChatGroq(
+                model=model_name,
+                api_key=self.api_keys["GROQ_API_KEY"],
+                temperature=temperature,
+            )
+            return llm
+        
+        else:
+            log.error("Unsupported LLM provider", provider=provider)
+            raise ValueError(f"Unsupported LLM provider: {provider}")
+        
+if __name__=="__main__":
+    loader = ModelLoader()
+    
+    # Test embedding model loading
+    embeddings=loader.load_embeddings()
+    print(f"Embedding Model Loaded: {embeddings}")
+    
+    # Test the ModelLoader
+    result=embeddings.embed_query("Hello, how are you?")
+    print(f"Embedding Result: {result}")
+    
+    # Test LLM loading based on YAML config
+    llm = loader.load_llm()
+    print(f"LLM Loaded: {llm}")
+    
+    # Test the ModelLoader
+    result=llm.invoke("Hello, how are you?")
+    print(f"LLM Result: {result.content}")
